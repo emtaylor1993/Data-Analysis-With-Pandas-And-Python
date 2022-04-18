68e4143017c206ba6175390b49d30764c942df5a
emtaylor1993 <etaylorsubs1993@gmail.com>
2022-04-17 21:43:29 -0400

    + added code, jupyter notebook for Python Crash Course

---
+++ python/Section-2-Python-Crash-Course/exercise1.py
@@ -0,0 +1,16 @@
+# Coding Exercise 1: Declare Variables
+
+# Declare a season variable and assign it a string of your favorite season (Summer, Fall, Spring, or Winter)
+# Make sure the first letter of the season is capitalized.
+
+# Declare a lucky_number variable and assign it to your favorite number.
+
+# Declare a hard_math variable and assign it to the sum of 3, 4 and 5 (HINT: use the + symbol to get Python
+# to add the numbesr for you)
+
+# Declare a my_str variable set to a string of "3".
+
+season = "Spring"
+lucky_number = 11
+hard_math = 3 + 4 + 5
+my_str = "3"
---
+++ python/Section-2-Python-Crash-Course/exercise2.py
@@ -0,0 +1,17 @@
+# Coding Exercise 2: Custom Functions
+
+# Define an easy_money function that accepts no parameters and always returns the value 100
+def easy_money():
+	return 100
+	
+# Define a best_food_ever function that accepts no parameters and always returns
+# the string "Sushi"
+def best_food_ever():
+	return "Sushi"
+	
+# Define a convert_to_currency function that accepts a single parameter (an integer)
+# The function should convert the argument to a string, prefix it with a dollar sign, and
+# return the results.
+def convert_to_currency(currency):
+	string = "$" + str(currency)
+	return string
